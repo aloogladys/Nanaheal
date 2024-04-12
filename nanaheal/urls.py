e0c684ae1490d527069fd270e9669df0b96eb9c0
aloogladys <aloogladys@gmail.com>
2024-04-12 19:56:39 +0300

    created and fixed about section

--- nanaheal/urls.py
+++ nanaheal/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from nana_app.views import index
+from nana_app.views import index, test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index),
+    path('test/',test)
     
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
